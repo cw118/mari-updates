c870e34fb3d59921c0bea92f496804d066428af8
cw118 <carolyn94118@gmail.com>
2022-01-12 18:14:18 -0500

    Add scraper scripts and initial Actions workflow

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# mari-updates
-Web scraper that searches for Marianopolis admissions updates and new calendar links. Updates daily.
+## Marianopolis College updates
+
+This runs on a web scraper built with Python and Beautiful Soup, which updates and writes to the README in this repo daily thanks to GitHub Actions automation.
+
+### Admissions updates
+
+Fall 2022 Admissions
+
+The online application system will open on Thursday, January 13, 2022. The deadline to apply is Tuesday, March 1, 2022 at 11:59 pm. Students will be able to upload any supporting admission documents via Omnivox.
+
+Admissions Information Evening
+
+Our virtual Admissions Information Evening will take place Monday, January 31, 2022, 4:00-7:00 p.m. This event is open to Secondary V students and their parents. More information to follow.
+
+### Calendars
+
+- Fall 2021 Academic Calendar: http://www.marianopolis.edu/wp-content/uploads/2021/08/Fall-2021-Academic-Calendar-August-5.pdf
+- Winter 2022 Academic Calendar: http://www.marianopolis.edu/wp-content/uploads/2021/10/Winter-2022-Academic-Calendar.pdf
+- Course Calendar 2021-2022: https://marianopolis-college.dcatalog.com/v/2021-2022-Calendar/
+- Course Calendar 2020-2021: https://marianopolis-college.dcatalog.com/v/2020-2021-Marianopolis-Calendar/
---
+++ main.py
@@ -0,0 +1,69 @@
+from bs4 import BeautifulSoup
+import requests
+
+def main():
+    scrape()
+
+"""
+Simple web scraper for Marianopolis College's admissions updates and calendars
+Modified from my web scraper built for LHD: Build day 4
+"""
+
+def scrape():
+    """
+    Scrape admissions updates
+    """
+    # Grab HTML of the corresponding site
+    url_adm = "https://www.bemarianopolis.ca/admissions/admissions-updates/"
+    html_adm = requests.get(url_adm).text
+
+    soup = BeautifulSoup(html_adm, "lxml") # Parse with LXML parser
+
+    """
+    The class list/selector used below is quite specific due to it being a WordPress site.
+    Depending on the site structure after new updates are posted, may need to be modified.
+    """
+    update_section = soup.find("div", class_ = "x-section e4336-11 m3cg-0 m3cg-3 m3cg-4")
+
+    # Search for paragraphs containing updates
+    updates = update_section.find_all("p")
+
+    """
+    Scrape calendars (links)
+    """
+    # Grab HTML of the corresponding site
+    url_cal = "https://www.marianopolis.edu/campus-life/calendar/"
+    html_cal = requests.get(url_cal).text
+
+    soup = BeautifulSoup(html_cal, "lxml") # Parse with LXML parser
+
+    calendar_section = soup.find("article", class_ = "content") # The page being scraped here only links to calendars within the article.content tags with a few exceptions
+
+    # Search for links in the content <article> (this should exclude any navbar and footer links)
+    calendars = calendar_section.find_all("a")
+
+    """
+    Open README file and write
+    """
+    with open("README.md", "w") as f:
+        # Write introduction and some headings
+        f.write("## Marianopolis College updates\n\n")
+        f.write("This runs on a web scraper built with Python and Beautiful Soup, which updates and writes to the README in this repo daily thanks to GitHub Actions automation.\n\n")
+        f.write("### Admissions updates\n\n")
+
+        # Write admissions updates
+        for update in updates:
+            f.write(update.text + "\n\n")
+
+        f.write("### Calendars\n\n")
+
+        for calendar in calendars:
+            calendar_url = calendar["href"] # Loop through each calendar link (anchor tag) and extract its href attribute (its URL)
+            calendar_title = calendar.text  # Save the "title" of the calendar
+
+            # Write calendar links (which all, hopefully, have the word "calendar" in their title at some point)
+            if "calendar" in calendar_title.lower():
+                f.write(f"- {calendar_title}: {calendar_url}\n") # Use a string literal for users to more easily identify what calendar each link leads to
+
+if __name__ == "__main__":
+    main()
