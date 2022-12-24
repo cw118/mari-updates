a2e6914c431ed7564489576a265064ddd3f2551e
Carolyn Wu <87150472+cw118@users.noreply.github.com>
2022-12-24 14:38:40 -0500

    chore(main.py): rm unused check_url()

--- main.py
+++ main.py
@@ -12,16 +12,6 @@ Simple web scraper for Marianopolis College's admissions updates, calendars and 
 Modified from my web scraper built for LHD: Build day 4 (see my lhd_build repo)
 """
 
-def check_url(url):
-    # Verify HTTP status codes returned by GET requests to URLs
-    if requests.get(url).status_code != 200:
-        with open("ERRORS.md", "w") as f:
-            # Write problematic links that don't return a 200 OK status to the ERRORS.md file
-            f.write(f"This link needs to be updated (moved or deleted): {url}\n")
-        return 1
-    else:
-        return 0
-
 def normalize(text):
     # Replace smart quotes/apostrophes and normalize unicode 
     # (unicodedata hasn't been replacing smart quotes or EN/EM dashes, so it's being done separately)
@@ -79,77 +69,76 @@ def scrape():
     """
     Open README file and write if URL checks for all links pass
     """
-    if (check_url(url_adm) == 0) and (check_url(url_cal) == 0):
-        with open("README.md", "w") as f:
-            # Set horizontal rule Markdown syntax to variable to print/write after each section
-            hr = "---\n\n"
+    with open("README.md", "w") as f:
+        # Set horizontal rule Markdown syntax to variable to print/write after each section
+        hr = "---\n\n"
 
-            # Write introduction, Web Scrape workflow badge and some headings
-            f.write(f"# Marianopolis College updates {year}\n\n")
-            f.write("[![Web Scrape](https://github.com/cw118/mari-updates/actions/workflows/scrape.yml/badge.svg)](https://github.com/cw118/mari-updates/actions/workflows/scrape.yml)\n\n")
-            f.write("This runs on a web scraper built with Python and Beautiful Soup, which updates and writes to the README in this repo once a day thanks to GitHub Actions automation.\n\n")
-            f.write("*Refer to [DOCS.md](DOCS.md) for this repository's documentation.*\n\n" + hr) # hr = horizontal rule
+        # Write introduction, Web Scrape workflow badge and some headings
+        f.write(f"# Marianopolis College updates {year}\n\n")
+        f.write("[![Web Scrape](https://github.com/cw118/mari-updates/actions/workflows/scrape.yml/badge.svg)](https://github.com/cw118/mari-updates/actions/workflows/scrape.yml)\n\n")
+        f.write("This runs on a web scraper built with Python and Beautiful Soup, which updates and writes to the README in this repo once a day thanks to GitHub Actions automation.\n\n")
+        f.write("*Refer to [DOCS.md](DOCS.md) for this repository's documentation.*\n\n" + hr) # hr = horizontal rule
 
-            # Write admissions updates, iterating over and checking all children of the section <div>
-            f.write("## [Admissions updates](https://www.bemarianopolis.ca/admissions/admissions-updates/)\n\n")
+        # Write admissions updates, iterating over and checking all children of the section <div>
+        f.write("## [Admissions updates](https://www.bemarianopolis.ca/admissions/admissions-updates/)\n\n")
 
-            # Iterate over all children of the update section <div>
-            for update in updates:         
-                # Write as <h3> if text is wrapped in <span> tags (the update "title")
-                # All spans used seem to have `style="text-decoration: underline;"`, meaning they should be emphasized
-                if update.find("span"):
-                    # Ensure there's more than a newline to avoid injecting empty formatting tags
-                    # If the text content contains more than a newline, make it a level 3 heading
-                    if update.text != "\n":
-                        update_text = normalize(update.text).strip()
-                        f.write(f"### {update_text}\n\n")
-                else:
+        # Iterate over all children of the update section <div>
+        for update in updates:         
+            # Write as <h3> if text is wrapped in <span> tags (the update "title")
+            # All spans used seem to have `style="text-decoration: underline;"`, meaning they should be emphasized
+            if update.find("span"):
+                # Ensure there's more than a newline to avoid injecting empty formatting tags
+                # If the text content contains more than a newline, make it a level 3 heading
+                if update.text != "\n":
                     update_text = normalize(update.text).strip()
-                    f.write(update_text + "\n\n")
-            # Suggest source link/page to readers as the scraper doesn't preserve rich text/hyperlinks
-            f.write("***\*\*Visit the [Marianopolis College website](https://www.bemarianopolis.ca/admissions/updates/) for details.***\n\n" + hr)
+                    f.write(f"### {update_text}\n\n")
+            else:
+                update_text = normalize(update.text).strip()
+                f.write(update_text + "\n\n")
+        # Suggest source link/page to readers as the scraper doesn't preserve rich text/hyperlinks
+        f.write("***\*\*Visit the [Marianopolis College website](https://www.bemarianopolis.ca/admissions/updates/) for details.***\n\n" + hr)
 
-            # Write calendar names and their corresponding links
-            f.write("## [Calendars](https://www.marianopolis.edu/campus-life/calendar/)\n\n")
-            f.write("Looking for Marianopolis' course and academic calendars? See the list below for past and current published calendars:\n\n")
+        # Write calendar names and their corresponding links
+        f.write("## [Calendars](https://www.marianopolis.edu/campus-life/calendar/)\n\n")
+        f.write("Looking for Marianopolis' course and academic calendars? See the list below for past and current published calendars:\n\n")
 
-            for calendar in calendars:
-                calendar_url = calendar["href"] # Loop through each calendar link (anchor tag) and extract its href attribute (its URL)
-                calendar_title = normalize(calendar.text)  # Save the "title" of the calendar
+        for calendar in calendars:
+            calendar_url = calendar["href"] # Loop through each calendar link (anchor tag) and extract its href attribute (its URL)
+            calendar_title = normalize(calendar.text)  # Save the "title" of the calendar
 
-                # Write calendar links (which all, hopefully, have the word "calendar" in their title at some point)
-                if "calendar" in calendar_title.lower():
-                    f.write(f"- *{calendar_title}:* {calendar_url}\n") # Use a string literal for users to more easily identify what calendar each link leads to
-            f.write("\n" + hr) # Newline and horizontal rule in between two sections
+            # Write calendar links (which all, hopefully, have the word "calendar" in their title at some point)
+            if "calendar" in calendar_title.lower():
+                f.write(f"- *{calendar_title}:* {calendar_url}\n") # Use a string literal for users to more easily identify what calendar each link leads to
+        f.write("\n" + hr) # Newline and horizontal rule in between two sections
 
-            # Write current year's admissions articles
-            f.write("## [Admission articles](https://www.bemarianopolis.ca/category/admissions/)\n\n")
-            f.write("Recent articles published by the Marianopolis staff and recruitment team. Click on the title(s) to read the full text:\n\n")
-            # Prepare table (head and separator)
-            f.write("| Article | Publish Date | Excerpt |\n")
-            f.write("| ------- | ------------ | ------- |\n")
+        # Write current year's admissions articles
+        f.write("## [Admission articles](https://www.bemarianopolis.ca/category/admissions/)\n\n")
+        f.write("Recent articles published by the Marianopolis staff and recruitment team. Click on the title(s) to read the full text:\n\n")
+        # Prepare table (head and separator)
+        f.write("| Article | Publish Date | Excerpt |\n")
+        f.write("| ------- | ------------ | ------- |\n")
 
-            # For loop with if condition that only retrieves information of articles published in current year
-            for article in articles:
-                if str(year) in (article.select_one("p.p-meta > span > time.entry-date").text):
-                    # Get entry title (the .strip removes newlines, tabs and returns to prevent Markdown rendering errors)
-                    article_title = normalize(article.find("h2", class_ = "entry-title").text).strip()
-                    article_excerpt = article.find("div", class_ = "entry-content excerpt") # Get excerpt div (contains links and snippets/summaries)
+        # For loop with if condition that only retrieves information of articles published in current year
+        for article in articles:
+            if str(year) in (article.select_one("p.p-meta > span > time.entry-date").text):
+                # Get entry title (the .strip removes newlines, tabs and returns to prevent Markdown rendering errors)
+                article_title = normalize(article.find("h2", class_ = "entry-title").text).strip()
+                article_excerpt = article.find("div", class_ = "entry-content excerpt") # Get excerpt div (contains links and snippets/summaries)
 
-                    article_pubdate = normalize(article.select_one("p.p-meta > span > time.entry-date").text) # Get publish date in text form
-                    article_link = article_excerpt.find("a")["href"].strip() # Get article link (scraped from the "Read More" link button in the excerpt divs)
-                    article_snippet = normalize(article_excerpt.find("p").text) # Get snippets/summaries of the articles
+                article_pubdate = normalize(article.select_one("p.p-meta > span > time.entry-date").text) # Get publish date in text form
+                article_link = article_excerpt.find("a")["href"].strip() # Get article link (scraped from the "Read More" link button in the excerpt divs)
+                article_snippet = normalize(article_excerpt.find("p").text) # Get snippets/summaries of the articles
 
-                    # Write article data to table row (Markdown)
-                    f.write(f"| [{article_title}]({article_link}) |{article_pubdate} | {article_snippet}|\n")
+                # Write article data to table row (Markdown)
+                f.write(f"| [{article_title}]({article_link}) |{article_pubdate} | {article_snippet}|\n")
 
-            # Retrieve and write (current) timestamp of the last scraper and URL check run
-            timestamp = datetime.now(timezone('America/Toronto')) # Convert to EST timezone (Toronto is a commonly used standard timezone that matches our purposes)
-            day = timestamp.strftime("%a %b. %d, %Y")
-            time = timestamp.strftime("%H:%M %p")
-            # Write horizontal rule before timestamp
-            f.write("\n" + hr)
-            f.write(f"*Last updated on {day} at {time} (EST).*")
+        # Retrieve and write (current) timestamp of the last scraper and URL check run
+        timestamp = datetime.now(timezone('America/Toronto')) # Convert to EST timezone (Toronto is a commonly used standard timezone that matches our purposes)
+        day = timestamp.strftime("%a %b. %d, %Y")
+        time = timestamp.strftime("%H:%M %p")
+        # Write horizontal rule before timestamp
+        f.write("\n" + hr)
+        f.write(f"*Last updated on {day} at {time} (EST).*")
 
 if __name__ == "__main__":
     main()
